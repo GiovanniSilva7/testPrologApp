6a3ab40e3851b0170cb7346c2fd8fa0ff7206752
Giovanni Genuino <giovanni.genuino@tcia.com.br>
2025-11-14 17:55:30 -0300

    chore:inicializando e configurando Spring

---
+++ src/main/java/com/giovanni/testePrologApp/TestePrologAppApplication.java
@@ -0,0 +1,13 @@
+package com.giovanni.testePrologApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePrologAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePrologAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovanni/testePrologApp/TestePrologAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanni.testePrologApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePrologAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
